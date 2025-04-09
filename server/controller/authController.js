78dc80e71d1f6982b843b9f50d40dbe51469b481
prafful sharma <sharmaprafful646@gmail.com>
2025-04-09 07:40:21 +0530

    Update authController.js

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -170,38 +170,7 @@ export const isAuthenticed = async (req, res)=>{
     }
 }
 
-//Send Password reset otp
-
-// export const sendResetOtp = async (req,res)=>{
-//     const {email}= req.body;
-//     if(!email){
-//         return res.json({success : false, message: 'Email is required'})
-//     }
-//     try{
-//         const user = await userModel.findOne({email});
-//         if(!user){
-//             return res.json({success : false, message : 'User not found '});
-//         }
-//         const otp = String(Math.floor(100000+ Math.random() * 900000));
-
-//         user.resetOtp = otp;
-//         user.resetOtpExpireAt = Date.now() + 15 *60 * 1000
-
-//         await user.save();
-        
-//         const mailOption = {
-//             from : process.env.SENDER_EMAIL,
-//             to: user.email,
-//             subject:'PassWord Reset  OTP', 
-//             text: `Your OTP for resetting your Password is ${otp}. Use this OTP to proceed with resetting your password.`
-//         };
-//         await transporter.sendMail(mailOption);
-
-//         return res.json({ success:true, message: 'OTP sent to your email '});
-//     }catch(error){
-//         return res.json({success: false, message: error.message});
-//     }
-// }
+ 
 
  
  
